4f1d2022ccd7ed03830b4b5464441e106d156ee5
Kinnear <ekinnear@devnet.local>
2025-11-21 07:32:48 -0800

    new port

--- run.sh
+++ run.sh
@@ -13,5 +13,5 @@ conda activate JobRecommender
 
 # export CUDA_VISIBLE_DEVICES=4,5,6,7
 
-CUDA_VISIBLE_DEVICES=4,5,6,7 srun --unbuffered torchrun --nproc_per_node=4 main.py --device 4,5,6,7 --model_type SplitBERT --version split_multigpu_overfit --batch_size 4 --epochs 100 --patience 100
+CUDA_VISIBLE_DEVICES=4,5,6,7 srun --unbuffered torchrun --master_port 29501 --nproc_per_node=4 main.py --device 4,5,6,7 --model_type SplitBERT --version split_multigpu_overfit --batch_size 4 --epochs 100 --patience 100
 # srun --unbuffered python main.py --device 0 --model_type SplitBERT --version split --batch_size 2
